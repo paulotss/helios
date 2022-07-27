655c8ac5ab6d59c7635e1e995a7a5c7428660b28
Paulo de Tarso <paulo.oinab@gmail.com>
2022-07-27 18:47:39 -0300

    Create route for Login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Login from './pages/Login';
 
 function App() {
   return (
-    <Login />
+    <Routes>
+      <Route path="/" element={ <Login /> } />
+    </Routes>
   );
 }
 
