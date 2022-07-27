33223724a62d146250cf682acf84009de08d3b4d
Paulo de Tarso <paulo.oinab@gmail.com>
2022-07-27 18:40:01 -0300

    Change for function components

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,72 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import useFormController from '../hooks/useFormController';
 
-class Login extends React.Component {
-  constructor() {
-    super();
-    this.handleChange = this.handleChange.bind(this);
-    this.loginClick = this.loginClick.bind(this);
-    this.state = {
-      email: '',
-      password: '',
-      isValid: false,
+const Login = () => {
+  const initialState = {
+    email: '',
+    password: '',
+  }
+  const [input, updateState] = useFormController(initialState);
+  const [isValid, setValid] = useState(false);
+  useEffect(() => {
+    const validateForm = () => {
+      if (input.password.length > 6 && input.email.match(/.+\@.+\..+/)) {
+        setValid(true);
+      } else if (isValid) {
+        setValid(false);
+      }
     }
-  }
-
-  handleChange({ target: { name, value } }) {
-    this.setState({
-      [name]: value,
-    }, (state) => {
-      const { email, password } = this.state;
-      const isValid = password.length > 6 && email.match(/.+\@.+\..+/);
-      this.setState({
-        isValid,
-      });
-    });
-  }
-
-  loginClick() {
-    const { history } = this.props;
-    history.push('/turmas');
-  }
-
-  render() {
-    const { email, password, isValid } = this.state;
-    return (
-      <div id="loginForm">
-        <h1>Login</h1>
-        <label>
-          Email
-          {' '}
-          <br />
-          <input
-            type="email"
-            name="email"
-            id="email"
-            value={ email }
-            onChange={ this.handleChange }
-          />
-        </label>
-        <label>
-          Password
-          {' '}
-          <br />
-          <input
-            type="password"
-            name="password"
-            id="password"
-            value={ password }
-            onChange={ this.handleChange }
-          />
-        </label>
-        <button
-          type='button'
-          disabled={ !isValid }
-        >
-          Entrar
-        </button>
-      </div>
-    );
-  }
+    validateForm();
+  }, [input]);
+  return (
+    <div id="loginForm">
+      <h1>Login</h1>
+      <label>
+        Email
+        {' '}
+        <br />
+        <input
+          type="email"
+          name="email"
+          id="email"
+          value={ input.email }
+          onChange={ updateState }
+        />
+      </label>
+      <label>
+        Password
+        {' '}
+        <br />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={ input.password }
+          onChange={ updateState }
+        />
+      </label>
+      <button
+        type='button'
+        disabled={ !isValid }
+      >
+        Entrar
+      </button>
+    </div>
+  );
 }
 
 export default Login;
