8e8d2e999c170d3c7fb2bcf581102bb91a97e55b
Paulo de Tarso <paulo.oinab@gmail.com>
2022-07-27 23:21:00 -0300

    Set props ClassCard

--- src/components/ClassCard.js
+++ src/components/ClassCard.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const ClassCard = () => {
+const ClassCard = ({ name, id }) => {
   return (
     <article>
       <div>
-        <img src="" alt="" />
+        <img src="https://nova-acropole.org.br/wp-content/uploads/2019/01/cropped-logonaface.png" alt="" />
       </div>
-      <h4>Class Name</h4>
+      <h4>{ `${id} | ${name}` }</h4>
     </article>
   );
 }
--- src/pages/Classes.js
+++ src/pages/Classes.js
@@ -1,10 +1,23 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
+import ClassCard from '../components/ClassCard';
 import AppContext from '../context/AppContext';
 
 const Classes = () => {
   const { classes } = useContext(AppContext);
   
-  console.log(classes);
+  return (
+    <div>
+      {
+        classes.map((classItem) => (
+          <ClassCard
+            key={ classItem.id }
+            name={ classItem.name }
+            id={ classItem.id }
+          />
+        ))
+      }
+    </div>
+  );
 }
 
 export default Classes;
