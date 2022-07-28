d0ac62d2f73ee0d5e2de8e44d4e38f9130e793a8
Paulo de Tarso <paulo.oinab@gmail.com>
2022-07-27 23:29:46 -0300

    Fix lint warnings

--- src/pages/Classes.js
+++ src/pages/Classes.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import ClassCard from '../components/ClassCard';
 import AppContext from '../context/AppContext';
 
