6fc907d339a27c74fd5cd90614e1f9e8b22ca1e7
Paulo de Tarso <paulo.oinab@gmail.com>
2022-08-13 10:49:52 -0300

    Populando helper classesData

--- src/components/ClassCard.js
+++ src/components/ClassCard.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const ClassCard = ({ name, id }) => {
+const ClassCard = ({ name, image, id }) => {
   return (
-    <article className="text-center">
+    <article className="text-center m-4">
       <div
         className="border-2 rounded-full w-40 h-40 m-auto bg-cover"
-        style={{backgroundImage: "url('https://nova-acropole.org.br/wp-content/uploads/2019/01/cropped-logonaface.png')"}}
+        style={{backgroundImage: `url('${ image }')`}}
       >
       </div>
       <h4 className="text-center mt-4">{ `${id} | ${name}` }</h4>
